995e1f886987defeafd50020536aead345ca15dd
Dylan Turner <dylantdmt@gmail.com>
2018-11-09 13:30:25 -0500

    Relative English -> Conlang complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # The-Conlang-Dictionary
 A dictionary app that allows you to take a word in English and get possible translations in an entered conlang.
+
+## Use
+To use, load a dictionary from a file, and then use the textbox below to get a translation of a word either from a source language to a conlang or vice versa. 
+
+If you would like to get the closest word to your string to translate (which I recommend unless you know specific words you've created w/out inflection, so most of the time), simply put a colon at the front of the word you want to translate.
--- dict.js
+++ dict.js
@@ -16,14 +16,97 @@ class Dictionary {
         this.Values.splice(key_index, 1);
     }
 
-    Translate(key) {
-        let key_index = this.Keys.findIndex((element) => element == key);
+    Translate(key, closest) {
+        let ret_translation = "";
+        
+        if(!closest) { // Find all exact matches
+            let key_index = this.Keys.findIndex((element) => element == key);
 
-        return this.Values[key_index];
+            let i = key_index + 1;
+            while(key_index != -1) {
+                ret_translation += ", " + this.Values[key_index];
+                
+                key_index = this.Keys.findIndex((element, index) => element == key && index >= i);
+                i = key_index + 1;
+            }
+
+            return ret_translation.substring(2);
+        } else { // Find closest word to match
+            ret_translation += "Closest matches: ";
+
+            let i = 0;
+            let distances = this.Keys.map((element) => LevensteinDistance(key, element));
+            let minInd = 0;
+            let minimum = distances.reduce((prev, dist, index) => {
+                if(index >= i) {
+                    minInd = dist < prev ? index : minInd;
+                    return   dist < prev ? dist : prev
+                }
+
+                minInd = -1;
+                return -1;
+            });
+
+            i = minInd + 1;
+            while(minInd != -1) {
+                ret_translation += "\n\t(" + this.Keys[minInd] + " => " + this.Values[minInd] + ")";
+
+                minimum = distances.reduce((prev, dist, index) => {
+                    if(index >= i) {
+                        minInd = dist < prev ? index : minInd;
+                        i = minInd + 1;
+
+                        return   dist < prev ? dist : prev
+                    }
+    
+                    minInd = -1;
+                    return -1;
+                });
+            }
+            
+            return ret_translation;
+        }
     }
 }
 
 var conDict = new Dictionary();
+
+function LevensteinDistance(input, comp) {
+    let n = input.length;
+    let m = comp.length;
+    let dist = [];
+    for(let i = 0; i < n + 1; i++) {
+        let row = [];
+        for(let j = 0; j < m + 1; j++)
+            row.push(0);
+        dist.push(row);
+    }
+
+    // Step 1
+    if (n == 0)
+        return m;
+    if (m == 0)
+        return n;
+
+    // Step 2
+    for (let i = 0; i <= n; dist[i][0] = i++);
+    for (let i = 0; i <= m; dist[0][i] = i++);
+
+    // Step 3
+    for (let i = 1; i <= n; i++) {
+        //Step 4
+        for (let j = 1; j <= m; j++) {
+            // Step 5
+            let cost = (comp[j - 1] == input[i - 1]) ? 0 : 1;
+
+            // Step 6
+            dist[i][j] = Math.min(Math.min(dist[i - 1][j] + 1, dist[i][j - 1] + 1), dist[i - 1][j - 1] + cost);
+        }
+    }
+
+    // Step 7
+    return dist[n][m];
+}
 
 function readDict(event) {
     let input = event.target;
@@ -48,7 +131,12 @@ function readDict(event) {
 
 function getWords() {
     let word = document.getElementById("tb_toConlang").value;
-    let translation = conDict.Translate(word);
+    let translation = "";
+
+    if(word[0] == ":")
+        translation = conDict.Translate(word.substring(1), true);
+    else
+        translation = conDict.Translate(word, false);
 
     let output = document.getElementById("output");
     output.innerText = translation;
